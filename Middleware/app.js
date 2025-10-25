dbe3edf2b64d89504c3b388920708a315474ba9d
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-10-25 18:37:40 +0600

    update

---
+++ Middleware/app.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express();
+app.use(express());
+
+const port = 3000;
+
+app.get('/',(req, res)=>{
+   res.send('hello express')
+})
+
+app.listen(port,console.log(`Server is running on port: ${port} `))
