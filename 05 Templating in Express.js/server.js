f65945e6b2fce73e378b29af2aeb1b043d620e61
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-10-29 17:15:35 +0600

    update

--- 05 Templating in Express.js/server.js
+++ 05 Templating in Express.js/server.js
@@ -4,8 +4,12 @@ const app = express();
 app.use(express());
 const PORT = 3000;
 
+// set the ejs as a view engine
+app.set('view engine', 'ejs');
+
 app.get('/',(req,res)=>{
-  res.send('hello from express')
+  const userName = 'Md. Sakib';
+  res.render('index',{userName})
 })
 
 app.listen(PORT, console.log(`server in running on port ${PORT}`))
