e96aa207122b79a49b2d1d26eaad040b4a9a459d
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-11-14 22:52:11 +0600

    add logic how to add  cookie and set the cookies time

--- 09 work with cookies in express/server.mjs
+++ 09 work with cookies in express/server.mjs
@@ -1,14 +1,30 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+
 
 const app = express();
 const PORT = 3000;
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Express.js server!');
+    //res.cookie('name', 'ExpressUser',{maxAge: 360000});
+    res.cookie('name', 'ExpressUser');
+    res.send('Welcome to the Express.js server!');
 });
 
+
+app.get('/fetch', (req, res) => {
+    console.log(req.cookies);
+    res.send('API Called')
+})
+
+
+
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
